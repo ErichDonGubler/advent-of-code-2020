56fb7ebf0953138e8543415fa6cdff98a7f87c1e
Erich Gubler <erichdongubler@gmail.com>
2020-12-02 21:22:53 -0700

    feat: solve day 2 part 2

--- tests/d02.rs
+++ tests/d02.rs
@@ -1,10 +1,10 @@
 use {
-    anyhow::Context,
+    anyhow::{anyhow, Context},
     itertools::Itertools,
     re_parse::ReParse,
     regex::Regex, // FIXME: file an upstream PR to get rid of the need for this
     serde::Deserialize,
-    std::{borrow::Cow, convert::TryInto, ops::RangeInclusive},
+    std::{borrow::Cow, convert::TryInto, num::NonZeroUsize, ops::RangeInclusive},
 };
 
 trait PasswordPolicy
@@ -106,6 +106,55 @@ fn part_1(s: &str) -> usize {
         .count()
 }
 
+#[derive(Debug, Eq, PartialEq)]
+struct ActualPasswordPolicy {
+    positions: [NonZeroUsize; 2],
+    character: char,
+}
+
+impl PasswordPolicy for ActualPasswordPolicy {
+    fn from_raw(lower: u8, upper: u8, character: char) -> anyhow::Result<Self> {
+        Ok(Self {
+            character,
+            positions: {
+                let parse_one_based_idx = |idx_name, idx| {
+                    NonZeroUsize::new(usize::from(idx)).ok_or_else(|| {
+                        anyhow!(
+                            "received position 0 for 1-based character {} index",
+                            idx_name
+                        )
+                    })
+                };
+                [
+                    parse_one_based_idx("first", lower)?,
+                    parse_one_based_idx("second", upper)?,
+                ]
+            },
+        })
+    }
+
+    fn validate(&self, password: &str) -> bool {
+        let Self {
+            character,
+            positions,
+        } = self;
+
+        password
+            .chars()
+            .zip((1..).map(|x| NonZeroUsize::new(x).unwrap()))
+            .filter(|(c, idx)| positions.contains(idx) && c == character)
+            .count()
+            == 1
+    }
+}
+
+fn part_2(s: &str) -> usize {
+    parse_password_policy_lines::<ActualPasswordPolicy>(s)
+        .filter_map(|res| res.ok())
+        .filter(|(pol, pw)| pol.validate(&pw))
+        .count()
+}
+
 const SAMPLE: &str = "\
 1-3 a: abcde
 1-3 b: cdefg
@@ -135,4 +184,36 @@ fn d02_p1_sample() {
 #[test]
 fn d02_p1_answer() {
     assert_eq!(part_1(INPUT), 603);
+}
+
+#[test]
+fn d02_p2_sample() {
+    assert_eq!(
+        parse_password_policy_lines::<ActualPasswordPolicy>(SAMPLE)
+            .filter_map(|res| res.ok())
+            .filter(|(pol, pw)| !pol.validate(&pw))
+            .collect_tuple::<(_, _)>(),
+        Some((
+            (
+                ActualPasswordPolicy {
+                    positions: [NonZeroUsize::new(1).unwrap(), NonZeroUsize::new(3).unwrap()],
+                    character: 'b',
+                },
+                "cdefg".into(),
+            ),
+            (
+                ActualPasswordPolicy {
+                    positions: [NonZeroUsize::new(2).unwrap(), NonZeroUsize::new(9).unwrap()],
+                    character: 'c',
+                },
+                "ccccccccc".into(),
+            )
+        )),
+    );
+    assert_eq!(part_2(SAMPLE), 1);
+}
+
+#[test]
+fn d02_p2_answer() {
+    assert_eq!(part_2(INPUT), 404);
 }
