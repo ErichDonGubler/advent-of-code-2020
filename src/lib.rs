b46643f0329ebae1a0bacc8f74c65409135354c9
Erich Gubler <erichdongubler@gmail.com>
2020-12-20 20:40:46 -0700

    feat: add `delta` module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,26 @@ pub mod days {
     automod::dir!("src/days/");
 }
 
+pub mod delta {
+    use std::{cmp::Ord, ops::Sub};
+
+    #[derive(Copy, Clone, Debug)]
+    pub enum Delta<T> {
+        Add(T),
+        Sub(T),
+    }
+
+    impl<T: Ord + Sub<Output = T>> Delta<T> {
+        pub fn new(old: T, new: T) -> Delta<T> {
+            if new > old {
+                Delta::Add(new - old)
+            } else {
+                Delta::Sub(old - new)
+            }
+        }
+    }
+}
+
 pub mod parsing {
     pub fn lines_without_endings(s: &str) -> impl Iterator<Item = &str> {
         s.lines().map(|l| {
